744d5674e677a5ddddf61f4b753f53baa527597d
norbertbraun <bnorbertjs@gmail.com>
2021-12-27 18:10:45 +0100

    feat: Fix callback call for basic measurement

--- src/measurements/basic.ts
+++ src/measurements/basic.ts
@@ -1,19 +1,15 @@
 import { EntryTransformer } from "../types";
 
-const getBasicMetrics = () => {
- return ['paint', 'largest-contentful-paint', 'first-input']
-}
-
-const transformEntry = (entry: PerformanceEntry) => entry
+const transformEntry = (name: string, entry: PerformanceEntry) => Object.assign(entry, { metricName: name })
 
 const basicPerformanceObserver = (name: string, performanceEntryHandler: EntryTransformer) => {
     try {
         const performanceObserver = new PerformanceObserver(entries => {
-            entries.getEntries().forEach(entry => transformEntry(entry))
+            entries.getEntries().forEach(entry => performanceEntryHandler(transformEntry(name, entry)))
         });
 		
         return () => {
-            performanceObserver.observe({ entryTypes: getBasicMetrics() })
+            performanceObserver.observe({ type: 'paint', buffered: true })
             return performanceObserver
         }
     } catch(error) {
